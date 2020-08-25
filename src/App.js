48ac9b827d6899fe62d097a8f1267358041132fb
jakdev047 <jakfreelancer047@gmail.com>
2020-08-25 19:54:30 +0600

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>Hello React</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
